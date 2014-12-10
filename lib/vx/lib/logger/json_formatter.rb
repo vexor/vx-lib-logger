bb26c5e36874c02c7ac2bcc046281406ec9a4559
Dmitry Galinsky <dima.exe@gmail.com>
2014-12-10 20:44:15 +0300

    using oj

--- lib/vx/lib/logger/json_formatter.rb
+++ lib/vx/lib/logger/json_formatter.rb
@@ -1,13 +1,49 @@
-require 'json'
+require 'oj'
 
 module Vx ; module Lib ; module Logger
 
   JsonFormatter = Struct.new(:parent) do
 
     def call(_, _, _, msg)
-      ::JSON.dump(msg) + "\n"
+      ::Oj.dump(msg, mode: :compat) + "\n"
     end
 
+=begin
+    def safe_value(value, options = {})
+      new_value = case value.class.to_s
+                  when "String", "Fixnum", "Float"
+                    value
+                  when "Symbol", "BigDecimal"
+                    value.to_s
+                  when "Array"
+                    value = value.map(&:to_s)
+                    options[:join_arrays] ? value.join("\n") : value
+                  when 'NilClass'
+                    nil
+                  else
+                    value.inspect
+                  end
+      if new_value.is_a?(String)
+        new_value.encode('UTF-8', {:invalid => :replace, :undef => :replace, :replace => '?'})
+      else
+        new_value
+      end
+    end
+
+    def make_safe_hash(msg, options = {})
+      msg.inject({}) do |acc, pair|
+        msg_key, msg_value = pair
+
+        if msg_key == :fields
+          acc[msg_key] = make_safe_hash(msg_value, join_arrays: true)
+        else
+          acc[msg_key] = safe_value(msg_value, options)
+        end
+        acc
+      end
+    end
+  end
+=end
   end
 
 end ; end ; end
--- lib/vx/lib/logger/version.rb
+++ lib/vx/lib/logger/version.rb
@@ -1,7 +1,7 @@
 module Vx
   module Lib
     module Logger
-      VERSION = "0.0.4"
+      VERSION = "0.0.5"
     end
   end
 end
